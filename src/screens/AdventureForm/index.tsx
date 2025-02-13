69a67ebdeaa9bb9bcb12c792bc2a0c736a9ff4ba
Pedro Mello <contato@cicatriz.dev>
2025-02-13 18:13:04 -0300

    4516 aula 1.2

---
+++ src/screens/AdventureForm/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const AdventureForm = () => {
+	return (
+		<View>
+			<Text>Adicionar aventura</Text>
+		</View>
+	);
+};
+
+export default AdventureForm;

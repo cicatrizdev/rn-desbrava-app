9b3a33da08abe62429cd85240877900c42790d5a
Pedro Mello <contato@cicatriz.dev>
2025-03-13 16:05:15 -0300

    4517 aula 4.1

--- src/screens/AdventureForm/index.tsx
+++ src/screens/AdventureForm/index.tsx
@@ -165,6 +165,8 @@ const AdventureForm = () => {
 					});
 					searchLocation(`${latitude}, ${longitude}`);
 				}}
+				zoomEnabled
+				zoomControlEnabled
 			>
 				{selectedLocation && (
 					<Marker
@@ -193,6 +195,25 @@ const AdventureForm = () => {
 					right={<TextInput.Icon icon='magnify' />}
 					onSubmitEditing={(event) => searchLocation(event.nativeEvent.text)}
 				/>
+			</View>
+			<View
+				style={{
+					position: 'absolute',
+					bottom: 50,
+					width: '100%',
+					alignItems: 'center',
+					justifyContent: 'center',
+					paddingHorizontal: 30,
+				}}
+			>
+				<Button
+					mode='contained'
+					onPress={() => setIsMapViewActive(false)}
+					style={{ backgroundColor: colors.primary }}
+					textColor={colors.black}
+				>
+					Confirmar Localização
+				</Button>
 			</View>
 		</>
 	);

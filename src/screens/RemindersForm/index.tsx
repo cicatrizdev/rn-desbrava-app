4efd8134dc40d8a7d81aea8bb72869eb575e2b74
Pedro Mello <contato@cicatriz.dev>
2025-04-26 16:26:54 -0300

    4518/4.2

--- src/screens/RemindersForm/index.tsx
+++ src/screens/RemindersForm/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 import AppHeader from '../../components/AppHeader';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackNavigationProp } from '../../navigation';
+import { TextInput } from 'react-native-paper';
 
 const RemindersForm = () => {
 	const navigation = useNavigation<RootStackNavigationProp>();
+	const [reminderForm, setReminderForm] = useState<>();
 
 	return (
 		<>
 			<AppHeader title='Adicionar lembrete' icon='close' onPress={() => navigation.goBack()} />
-			<Text>RemindersForm</Text>
+			<ScrollView>
+				<TextInput
+					placeholder='Título'
+					value={reminderForm.title}
+					onChangeText={(text) => setTitle(text)}
+				/>
+			</ScrollView>
 		</>
 	);
 };
